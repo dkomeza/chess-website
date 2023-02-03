1623fee54c374c280dc2193074b7b1c389c8d52b
Dawid Komęza <dawid.komeza@gmail.com>
2023-02-03 16:10:59 +0000

    Added refs for the signup page

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from "react";
+
+import { useAuth } from "@/contexts/AuthContext";
 
 function Signup() {
+  const { currentUser } = useAuth();
+  const nameRef = useRef<HTMLInputElement>(null);
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+  }
+
   return (
-    <div>Signup</div>
-  )
+    <div>
+      <h1>Signup</h1>
+      <form>
+        <input type="text" ref={nameRef} />
+        <input type="email" ref={emailRef} />
+
+        <input type="password" ref={passwordRef} />
+        <button type="submit">Signup</button>
+      </form>
+    </div>
+
+  );
 }
 
-export default Signup
+export default Signup;
--- frontend/src/routers/InnerRouter.tsx
+++ frontend/src/routers/InnerRouter.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as _Router, Routes, Route } from "react-router-dom";
 
 function InnerRouter() {
   return (
