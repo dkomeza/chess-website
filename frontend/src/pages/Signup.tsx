fec9fedcdf32d67692a06482d4cdeb26696cf8d5
Dawid Komęza <dawid.komeza@gmail.com>
2023-02-03 19:02:48 +0000

    Initial signup page

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -3,27 +3,35 @@ import React, { useEffect, useRef, useState } from "react";
 import { useAuth } from "@/contexts/AuthContext";
 
 function Signup() {
-  const { currentUser } = useAuth();
+  const { currentUser, signup } = useAuth();
   const nameRef = useRef<HTMLInputElement>(null);
   const emailRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
 
-  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
+    try {
+      await signup(
+        nameRef.current!.value,
+        emailRef.current!.value,
+        passwordRef.current!.value
+      );
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   return (
     <div>
       <h1>Signup</h1>
-      <form>
-        <input type="text" ref={nameRef} />
-        <input type="email" ref={emailRef} />
+      <form onSubmit={handleSubmit}>
+        <input type="text" ref={nameRef} required />
+        <input type="email" ref={emailRef} required />
 
-        <input type="password" ref={passwordRef} />
+        <input type="password" ref={passwordRef} required />
         <button type="submit">Signup</button>
       </form>
     </div>
-
   );
 }
 
